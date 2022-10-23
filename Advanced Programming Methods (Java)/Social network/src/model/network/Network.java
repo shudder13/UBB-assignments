e3b45a88478083eab8449b26ff9f735885ca3baf
Marius Sterian <marius.sterian13@gmail.com>
2022-10-23 20:54:19 +0300

    feat(network): display number of communities

---
+++ Advanced Programming Methods (Java)/Social network/src/model/network/Network.java
@@ -0,0 +1,15 @@
+package model;
+
+import java.util.Collection;
+
+public class Network {
+    private boolean[][] graph;
+
+    public Network(Collection<User> users, Collection<Friendship> friendships) {
+
+    }
+
+    public Integer getNumberOfCommunities() {
+        return -1;
+    }
+}
---
+++ Advanced Programming Methods (Java)/Social network/src/model/network/NetworkNode.java
@@ -0,0 +1,2 @@
+package model.network;public class NetworkNode {
+}
